abe48698b7e4430a664313d2c935b61aabf91480
Marc Mosko <mmosko@parc.com>
2017-03-21 16:51:01 -0700

    build of all documents.

--- src/.targets.mk
+++ src/.targets.mk
@@ -9,8 +9,14 @@ draft-mosko-icnrg-cachecontrol-00.xml: draft-mosko-icnrg-cachecontrol.xml
 	sed -e 's/draft-mosko-icnrg-cachecontrol-latest/draft-mosko-icnrg-cachecontrol-00/' $< > $@
 draft-mosko-icnrg-ccnxchunking-00.xml: draft-mosko-icnrg-ccnxchunking.xml
 	sed -e 's/draft-mosko-icnrg-ccnxchunking-latest/draft-mosko-icnrg-ccnxchunking-00/' $< > $@
-draft-mosko-icnrg-ccnxuri-00.xml: draft-mosko-icnrg-ccnxuri.xml
-	sed -e 's/draft-mosko-icnrg-ccnxuri-latest/draft-mosko-icnrg-ccnxuri-00/' $< > $@
+draft-mosko-icnrg-ccnxserialversion-00.xml: draft-mosko-icnrg-ccnxserialversion.xml
+	sed -e 's/draft-mosko-icnrg-ccnxserialversion-latest/draft-mosko-icnrg-ccnxserialversion-00/' $< > $@
+draft-mosko-icnrg-ccnxtimeversion-00.xml: draft-mosko-icnrg-ccnxtimeversion.xml
+	sed -e 's/draft-mosko-icnrg-ccnxtimeversion-latest/draft-mosko-icnrg-ccnxtimeversion-00/' $< > $@
+draft-mosko-icnrg-ccnxurischeme-00.xml: draft-mosko-icnrg-ccnxurischeme.xml
+	sed -e 's/draft-mosko-icnrg-ccnxurischeme-latest/draft-mosko-icnrg-ccnxurischeme-00/' $< > $@
+draft-mosko-icnrg-selectors-00.xml: draft-mosko-icnrg-selectors.xml
+	sed -e 's/draft-mosko-icnrg-selectors-latest/draft-mosko-icnrg-selectors-00/' $< > $@
 draft-tschudin-icnrg-flic-00.xml: draft-tschudin-icnrg-flic.xml
 	sed -e 's/draft-tschudin-icnrg-flic-latest/draft-tschudin-icnrg-flic-00/' $< > $@
 draft-wood-icnrg-ccnxkeyexchange-00.xml: draft-wood-icnrg-ccnxkeyexchange.xml
@@ -27,7 +33,13 @@ diff-draft-wood-icnrg-ccnxkeyexchange-.txt.html: draft-wood-icnrg-ccnxkeyexchang
 	-$(rfcdiff) --html --stdout $^ > $@
 diff-draft-tschudin-icnrg-flic-.txt.html: draft-tschudin-icnrg-flic-.txt draft-tschudin-icnrg-flic.txt
 	-$(rfcdiff) --html --stdout $^ > $@
-diff-draft-mosko-icnrg-ccnxuri-.txt.html: draft-mosko-icnrg-ccnxuri-.txt draft-mosko-icnrg-ccnxuri.txt
+diff-draft-mosko-icnrg-selectors-.txt.html: draft-mosko-icnrg-selectors-.txt draft-mosko-icnrg-selectors.txt
+	-$(rfcdiff) --html --stdout $^ > $@
+diff-draft-mosko-icnrg-ccnxurischeme-.txt.html: draft-mosko-icnrg-ccnxurischeme-.txt draft-mosko-icnrg-ccnxurischeme.txt
+	-$(rfcdiff) --html --stdout $^ > $@
+diff-draft-mosko-icnrg-ccnxtimeversion-.txt.html: draft-mosko-icnrg-ccnxtimeversion-.txt draft-mosko-icnrg-ccnxtimeversion.txt
+	-$(rfcdiff) --html --stdout $^ > $@
+diff-draft-mosko-icnrg-ccnxserialversion-.txt.html: draft-mosko-icnrg-ccnxserialversion-.txt draft-mosko-icnrg-ccnxserialversion.txt
 	-$(rfcdiff) --html --stdout $^ > $@
 diff-draft-mosko-icnrg-ccnxchunking-.txt.html: draft-mosko-icnrg-ccnxchunking-.txt draft-mosko-icnrg-ccnxchunking.txt
 	-$(rfcdiff) --html --stdout $^ > $@
